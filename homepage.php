0deeb3c258aa09eefd9dc0aae85fffaa4be37cce
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-12 12:56:55 -0700

    boom

--- homepage.php
+++ homepage.php
@@ -17,13 +17,13 @@ Template Name: Home Page
 				<div class="col_4 rounded shadow transparent">
 					<?php $News_id = 197; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="col_4 rounded shadow transparent">
+				<div class="pre_4 col_4 rounded shadow transparent">
 					<?php $News_id = 219; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="col_4 rounded shadow  transparent">
+				<div class="pre_4 col_4 rounded shadow transparent">
 					<?php $News_id = 121; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="col_4 rounded shadow last transparent">
+				<div class="pre_8 col_4 rounded shadow last transparent">
 					<?php $News_id = 123; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 			</div>

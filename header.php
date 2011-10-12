9f4bb7d3e376c08f05cfcf32085601e47586d9c2
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-12 11:46:00 -0700

    form fixin'

--- header.php
+++ header.php
@@ -121,7 +121,7 @@
 
 		<header id="header" class="row transparent shadow rounded">
 			<div class="col_4"><a href="<?php echo get_option('home'); ?>/"><img src="<?php bloginfo('template_directory'); ?>/images/logo.png"</a></div>
-			<h1 style="display:none;"><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<h1 style="display:none;"><a href="<?php echo get_option('home'); ?>/homepage.php"><?php bloginfo('name'); ?></a></h1>
 			<div style="display:none"; class="description"><?php bloginfo('description'); ?></div>
 			<div id="social" class="col_4">
 				<ul>

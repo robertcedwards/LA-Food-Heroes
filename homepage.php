518af34f283f686b964b069cf40807cab406b658
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-11 17:30:18 -0700

    reseting

---
+++ homepage.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+
+			<div class="entry row">
+				<div class="col_4">
+					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
+				</div>			
+			</div>
+
+			<footer class="postmetadata">
+				<?php the_tags('Tags: ', ', ', '<br />'); ?>
+				Posted in <?php the_category(', ') ?> | 
+				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+			</footer>
+
+		</article>
+
+	<?php endwhile; ?>
+
+	<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
+
+	<?php else : ?>
+
+		<h2>Not Found</h2>
+
+	<?php endif; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,37 +1,24 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			
+		<article class="post" id="post-<?php the_ID(); ?>">
 
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
+			<div class="entry">
 
-			<div class="entry row">
-				<div class="col_4">
-					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
-				</div>			
+				<?php the_content(); ?>
+
+				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
 			</div>
 
-			<footer class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</footer>
+			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 
 		</article>
+		
+		<?php comments_template(); ?>
 
-	<?php endwhile; ?>
+		<?php endwhile; endif; ?>
 
-	<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
-
-	<?php else : ?>
-
-		<h2>Not Found</h2>
-
-	<?php endif; ?>
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>

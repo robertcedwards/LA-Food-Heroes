3c524461ea45d22dedc39b66f375349a22b4cf59
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-11 18:53:12 -0700

    deleted footer

--- homepage.php
+++ homepage.php
@@ -24,11 +24,6 @@ Template Name: Home Page
 				</div>
 			</div>
 
-			<footer class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</footer>
 
 		</article>
 

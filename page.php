638695c2c9c793d5b88f6fbbfd7bdd4a94fa4493
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-11 17:28:56 -0700

    changing page structure

--- page.php
+++ page.php
@@ -1,24 +1,37 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-		<article class="post" id="post-<?php the_ID(); ?>">
 
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-			<div class="entry">
+			<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 
-				<?php the_content(); ?>
-
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-
+			<div class="entry row">
+				<div class="col_4">
+					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
+				</div>			
 			</div>
 
-			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+			<footer class="postmetadata">
+				<?php the_tags('Tags: ', ', ', '<br />'); ?>
+				Posted in <?php the_category(', ') ?> | 
+				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+			</footer>
 
 		</article>
-		
-		<?php comments_template(); ?>
 
-		<?php endwhile; endif; ?>
+	<?php endwhile; ?>
 
+	<?php include (TEMPLATEPATH . '/_/inc/nav.php' ); ?>
+
+	<?php else : ?>
+
+		<h2>Not Found</h2>
+
+	<?php endif; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>

c157ff90ffd9e2440745af54ab4f96ab3a1955d6
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-12 13:19:12 -0700

    columns

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@ Template Name: Home Page
 
 			<h2 style="display:none;"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 			<div class="entry col_12 ">
-				<div class="col_4 rounded shadow transparent clearleft">
+				<div class="col_4 rounded shadow transparent">
 					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 				<div class="col_4 rounded shadow transparent">
@@ -20,10 +20,11 @@ Template Name: Home Page
 				<div class="col_4 rounded shadow transparent">
 					<?php $News_id = 219; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="col_4 rounded shadow omega transparent">
+
+				<div class="col_4 rounded shadow last transparent">
 					<?php $News_id = 121; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="col_4 rounded shadow omega transparent">
+				<div class="col_4 rounded shadow last transparent">
 					<?php $News_id = 123; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 			</div>

9d4649a54e873b16aa638d2f5c4bc2ba40aa4cdc
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-12 13:04:47 -0700

    layout

--- homepage.php
+++ homepage.php
@@ -11,19 +11,19 @@ Template Name: Home Page
 
 			<h2 style="display:none;"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 			<div class="entry col_12 ">
-				<div class="col_4 rounded shadow transparent">
+				<div class="col_4 rounded shadow transparent clearleft">
 					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 				<div class="col_4 rounded shadow transparent">
 					<?php $News_id = 197; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="pre_4 col_4 rounded shadow transparent">
+				<div class="col_4 rounded shadow transparent">
 					<?php $News_id = 219; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="pre_4 col_4 rounded shadow transparent">
+				<div class="col_4 rounded shadow omega transparent">
 					<?php $News_id = 121; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
-				<div class="pre_8 col_4 rounded shadow last transparent">
+				<div class="col_4 rounded shadow omega transparent">
 					<?php $News_id = 123; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 			</div>

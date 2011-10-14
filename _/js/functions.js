e1bc9a31d1ba8a4850246396dbc57f5ecce4a907
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-13 18:38:07 -0700

    adding blueberry

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,3 +1,12 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+$('.blueberry').blueberry();
+});
 
 
 /* optional triggers

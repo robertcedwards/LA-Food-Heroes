b45d980dd87cbc3a8199924f933fe6f24f40b02b
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-16 11:20:33 -0700

    added positive class to first column

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@ Template Name: Home Page
 
 			<h2 style="display:none;"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 			<div class="entry col_12 ">
-				<div class="col_4 rounded shadow transparent">
+				<div class="col_4 rounded shadow transparent positive">
 					<?php $News_id = 4; $page_data = get_page( $News_id  ); $content = apply_filters('the_content', $page_data->post_content); $title = $page_data->post_title; echo $content;?>
 				</div>
 				<div class="col_4 rounded shadow transparent">

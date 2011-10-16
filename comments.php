5dd3b4cfdaadd051349080b2435b33bb75107300
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-16 13:43:11 -0700

    added fbcomments

--- comments.php
+++ comments.php
@@ -1,1 +1,1 @@
-<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:comments href="lafoodheroes.com" num_posts="10" width="600"></fb:comments>
+<div class="fbcomments"><div classid="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:comments href="lafoodheroes.com" num_posts="10" width="600"></fb:comments></div>

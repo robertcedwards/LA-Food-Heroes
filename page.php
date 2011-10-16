feb6dcd966604358cbf85321cdd1af551bd9cac1
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-16 11:36:04 -0700

    page position fixing for logo

--- page.php
+++ page.php
@@ -6,11 +6,11 @@ Template Name: Pages
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<article class="post row rounded transparent positive" id="post-<?php the_ID(); ?>">
+		<article class="post row   " id="post-<?php the_ID(); ?>">
 
 
 
-			<div class="entry col_8">
+			<div class="entry col_8 transparent positive rounded">
 
 				<?php the_content(); ?>
 

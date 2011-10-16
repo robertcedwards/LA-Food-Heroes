e53eff634bad694ed00531d024282a00226975a6
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-16 11:44:49 -0700

    fixing padding issues

--- page.php
+++ page.php
@@ -7,20 +7,11 @@ Template Name: Pages
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<article class="post row" id="post-<?php the_ID(); ?>">
-
-
-
 			<div class="entry col_8 transparent positive rounded">
-
 				<?php the_content(); ?>
-
 				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-
+				<?php comments_template(); ?>
 			</div>
 		<?php get_sidebar(); ?>
-
-		<?php comments_template(); ?>
-
 		</article>
-
-		<?php endwhile; endif; ?>
+	<?php endwhile; endif; ?>

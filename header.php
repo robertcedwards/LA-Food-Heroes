c108f54d17843d0c6da3e12831e19b0040ddb9df
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-16 14:22:07 -0700

    added mobile class

--- header.php
+++ header.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
-
 <!--[if lt IE 7 ]> <html class="ie ie6 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7 ]>    <html class="ie ie7 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8 ]>    <html class="ie ie8 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 9 ]>    <html class="ie ie9 no-js" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <!-- the "no-js" class is for Modernizr. -->
-
 <head id="www-lafoodheroes-org" data-template-set="html5-reset-wordpress-theme" profile="http://gmpg.org/xfn/11">
-
-	<meta charset="<?php bloginfo('charset'); ?>">
-	
+	<meta charset="<?php bloginfo('charset'); ?>">	
 	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
 	<?php if (is_search()) { ?>
 	<meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
-
 	<title>
 		   <?php
 		      if (function_exists('is_tag') && is_tag()) {
@@ -38,7 +32,6 @@
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
-	
 	<meta name="title" content="<?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -58,40 +51,18 @@
 		         echo ' - page '. $paged; }
 		   ?>">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
-	
 	<meta name="google-site-verification" content="">
 	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
 	
 	<meta name="author" content="Robert C Edwards">
 	<meta name="Copyright" content="Copyright LA Food Heroes 2011. All Rights Reserved.">
-
 	<!-- Dublin Core Metadata : http://dublincore.org/ -->
 	<meta name="DC.title" content="LA Food Heroes">
 	<meta name="DC.subject" content="Food Heroes is a 501(c)(3) Non Profit Organization dedicated to improving the health of our communities through our relationship to food.">
 	<meta name="DC.creator" content="We Creative">
-	
-	<!--  Mobile Viewport meta tag
-	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
-	device-width : Occupy full width of the screen in its current orientation
-	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width -->
-	<!-- Uncomment to use; use thoughtfully!
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-	-->
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">	
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK
-		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
-		 
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
-	<!-- The is the icon for iOS's Web Clip.
-		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
-		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
-		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
-	
-
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/css/columnal.css">
 <!-- Fixes for IE -->
 	<!--[if lt IE 9]>
@@ -110,14 +81,9 @@
 	<![endif]-->
   <!-- End fixes for IE -->	<!-- CSS: screen, mobile & print are all in the same file -->
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
-
-	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
-	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-
 	<?php wp_head(); ?>
 	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 
@@ -127,11 +93,11 @@
   js.src = "//connect.facebook.net/en_US/all.js#appId=262586560424891&xfbml=1";
   d.getElementsByTagName('head')[0].appendChild(js);
 }(document));</script>
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/js/blueberry.css">
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
-
 </head>
 
 <body <?php body_class(); ?>>
@@ -146,7 +112,7 @@
 					<li class="twitter"><a href="#"><div class="social "><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div></a></li>
 				</ul>			
 			</div>
-			<div id="nav" class="col_6 last"><?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => '' ) ); ?></div>
+			<div id="nav" class="col_6 last mobile"><?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => '' ) ); ?></div>
 			<div id="searchform" class=" col_4 rounded transparent last">
 				<?php get_search_form( $echo ); ?>
 			</div>
